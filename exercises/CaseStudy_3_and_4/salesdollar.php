4aa6d57bbeb2d0fa36d7b59c8ec6fc41622e8acd
Dhruv Shanbhag <DHRUV008@e.ntu.edu.sg>
2021-10-18 00:08:30 +0800

    Updated Sales dollar and Sales Quantity. Thanks Yoong for being SQl god. Hehe

--- exercises/CaseStudy_3_and_4/salesdollar.php
+++ exercises/CaseStudy_3_and_4/salesdollar.php
@@ -15,49 +15,19 @@
                 exit;
             }
 
-            $sql="SELECT case4Drinks.drinkID, case4Drinks.drinkName, case4Drinks.price, case4Orders.quantity
-            FROM case4Drinks
-            LEFT JOIN case4Orders on case4Drinks.drinkID = case4Orders.drinkID
-            ORDER BY case4Drinks.drinkID";
+            $sql="SELECT drinkName, SUM(price * quantity) AS totalSales FROM case4Orders
+                  LEFT JOIN case4Drinks ON case4Orders.drinkID = case4Drinks.drinkID
+                  GROUP BY case4Drinks.drinkID";
             $result = $db->query($sql);
             
             if ($result->num_rows > 0) {
-                $subtotalJava = 0;
-                $subtotalLaitSingle = 0;
-                $subtotalLaitDouble = 0;
-                $subtotalCapSingle = 0;
-                $subtotalCapDouble = 0;
-                $subtotal = 0;
-                // output data of each row
+              echo '<table><tr><th align="left">Product Name&nbsp;</th><th>&nbsp;Sales($)</th></tr>';
                 while($row = $result->fetch_assoc()) {
-                  
-                  $product = $row["drinkID"];
-                  if ($product == 1 ){ 
-                      $subtotalJava += $row["quantity"] * $row["price"];
-                  }
-                  elseif($product == 2){
-                      $subtotalLaitSingle += $row["quantity"] * $row["price"];
-                  }
-                  elseif($product == 3){
-                    $subtotalLaitDouble += $row["quantity"] * $row["price"];
-                }
-                  elseif($product == 4){
-                    $subtotalCapSingle += $row["quantity"] * $row["price"];
-                }
-                  elseif($product == 5){
-                    $subtotalCapDouble += $row["quantity"] * $row["price"];
-                }
-                }
-                $subtotal = $subtotalJava + $subtotalLaitSingle + $subtotalLaitDouble + $subtotalCapSingle + $subtotalCapDouble;
-                echo "<table><tr><th>Product Name&nbsp;</th><th>&nbsp;Sales</th></tr>";
-                echo "<tr><td>Just Java </td> <td>&nbsp;" .$subtotalJava. "</td></tr>";
-                echo "<tr><td>Single Shot Cafe au Lait&nbsp;</td> <td>&nbsp;" .$subtotalLaitSingle. "</td></tr>";
-                echo "<tr><td>Double Shot Cafe au Lait&nbsp;</td> <td>&nbsp;" .$subtotalLaitDouble. "</td></tr>";
-                echo "<tr><td>Single Shot Iced Cappucino&nbsp;</td> <td>&nbsp;" .$subtotalCapSingle. "</td></tr>";
-                echo "<tr><td>Double Shot Iced Cappucino&nbsp;</td> <td>&nbsp;" .$subtotalCapDouble. "</td></tr>";             
-                echo "<tr><td><strong>Sum</strong>&nbsp;</td> <td>&nbsp;<strong>" .$subtotal. "</strong></td></tr>";
-                echo "</table>";
+                echo '<tr><td>' .$row["drinkName"]. '&nbsp;</td><td align="center">&nbsp;' .$row["totalSales"]. "</td></tr>"; 
+                     
               } 
+              echo "</table>"; 
+            }
             else {
                 echo "0 results";
               }
