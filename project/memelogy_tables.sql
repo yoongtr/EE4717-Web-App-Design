981d8d8ebf708ce91ff44da08b8219b974209753
Dhruv Shanbhag <DHRUV008@e.ntu.edu.sg>
2021-10-23 10:19:24 +0800

    Modified sql as it wasn't working, added dbconnect.php

--- project/memelogy_tables.sql
+++ project/memelogy_tables.sql
@@ -1,35 +1,40 @@
 USE f32ee;
 
+-- Tested adding these tables to mySQL, they work weeee
+
 CREATE TABLE users (
     UserID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FullName VARCHAR NOT NULL,
-    ProfileImage LONGBLOB NOT NULL,
-    Username VARCHAR NOT NULL,
-    Email VARCHAR NOT NULL,
-    UserPassword VARCHAR NOT NULL
+    FullName CHAR(40),
+    ProfileImage LONGBLOB,
+    Username VARCHAR(20) NOT NULL,
+    Email VARCHAR(40) NOT NULL,
+    UserPassword VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE products (
-    ProductSKU VARCHAR NOT NULL PRIMARY KEY,
-    ProductName INT UNSIGNED NOT NULL,
+    ProductSKU VARCHAR(20) NOT NULL PRIMARY KEY,
+    ProductName CHAR(40) NOT NULL,
     ProductImage LONGBLOB NOT NULL,
-    ProductDescription VARCHAR NOT NULL,
+    ProductDescription VARCHAR(100) NOT NULL,
     Price INT UNSIGNED NOT NULL,
-    ProductCategory VARCHAR NOT NULL,
+    ProductCategory VARCHAR(20) NOT NULL,
     Quantity INT UNSIGNED NOT NULL
 );
-
+ 
 CREATE TABLE orders (
     OrderID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ProductSKU VARCHAR NOT NULL FOREIGN KEY REFERENCES products(ProductSKU),
-    UserID INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES users(UserID),
+    ProductSKU VARCHAR(20) NOT NULL, 
+    UserID INT UNSIGNED NOT NULL, 
     OrderQuantity INT UNSIGNED NOT NULL,
-    OrderStatus VARCHAR,
+    OrderStatus VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ProductSKU) REFERENCES products(ProductSKU),
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
 );
 
 CREATE TABLE jobs (
     JobSubmissionID INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    UserID INT UNSIGNED NOT NULL FOREIGN KEY REFERENCES users(UserID),
+    UserID INT UNSIGNED NOT NULL, 
     StartDate DATE NOT NULL,
-    Experience VARCHAR NOT NULL
+    Experience VARCHAR(100) NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
 );
